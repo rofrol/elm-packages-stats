1f9107d4fce2dbeac4f577d735da4170bbe361f5
Joe Fiorini <joe@joefiorini.com>
2018-02-14 23:05:18 -0500

    Add a basic chart with a bit of fake data

--- client/Main.elm
+++ client/Main.elm
@@ -1,6 +1,31 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import LineChart
+import LineChart.Axis as Axis
+import LineChart.Area as Area
+import LineChart.Junk as Junk
+import LineChart.Dots as Dots
+import LineChart.Grid as Grid
+import LineChart.Dots as Dots
+import LineChart.Line as Line
+import LineChart.Colors as Colors
+import LineChart.Events as Events
+import LineChart.Legends as Legends
+import LineChart.Container as Container
+import LineChart.Interpolation as Interpolation
+import LineChart.Axis.Intersection as Intersection
+import Date exposing (Date)
+import Time exposing (Time)
+
+
+-- MOCK DATA
+
+
+type alias Entry =
+    { date : Time
+    , count : Float
+    }
 
 
 type Msg
@@ -8,13 +33,13 @@ type Msg
 
 
 type alias Model =
-    { packageCounts : List Int
+    { packageCounts : List Entry
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { packageCounts = [] }
+    ( { packageCounts = [ { date = 10 * Time.second, count = 222 } ] }
     , Cmd.none
     )
 
@@ -26,9 +51,32 @@ update msg model =
             model ! []
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
-    section [] [ text "blah" ]
+    section [] [ chart model.packageCounts ]
+
+
+chartConfig : List Entry -> LineChart.Config Entry Msg
+chartConfig entries =
+    { y = Axis.default 450 "Count" .count
+    , x = Axis.time 1270 "Date" .date
+    , container = Container.default "line-chart-1"
+    , interpolation = Interpolation.default
+    , intersection = Intersection.default
+    , legends = Legends.default
+    , events = Events.default
+    , junk = Junk.default
+    , grid = Grid.default
+    , area = Area.default
+    , line = Line.default
+    , dots = Dots.default
+    }
+
+
+chart : List Entry -> Html Msg
+chart entries =
+    LineChart.viewCustom (chartConfig entries)
+        [ LineChart.line Colors.blue Dots.circle "Packages" entries ]
 
 
 subscriptions : Model -> Sub Msg
