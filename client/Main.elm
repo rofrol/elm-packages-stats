ddbd564e3a2987b77e30891ffa7add7ad63edeae
Joe Fiorini <joe@joefiorini.com>
2018-02-16 16:59:06 -0500

    Start implementing elm-css

--- client/Main.elm
+++ client/Main.elm
@@ -1,56 +1,23 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import LineChart
-import LineChart.Axis as Axis
-import LineChart.Area as Area
-import LineChart.Junk as Junk
-import LineChart.Dots as Dots
-import LineChart.Grid as Grid
-import LineChart.Dots as Dots
-import LineChart.Line as Line
-import LineChart.Colors as Colors
-import LineChart.Events as Events
-import LineChart.Legends as Legends
-import LineChart.Container as Container
-import LineChart.Interpolation as Interpolation
-import LineChart.Axis.Intersection as Intersection
+import Html
+import Css exposing (..)
+import Html.Styled exposing (..)
+import Svg.Styled as Svg
+import Html.Styled.Attributes exposing (css)
 import Date exposing (Date, toTime)
 import Http
-import Json.Decode exposing (Decoder, field, list, float)
-import Json.Decode.Extra exposing (date)
+import Model exposing (Entry, Model, decodePackageCounts)
+import Chart exposing (chart)
+import Svg
 
 
 -- MOCK DATA
 
 
-type alias Entry =
-    { date : Date
-    , count : Float
-    }
-
-
 type Msg
     = RequestPackageCounts
     | LoadPackageCounts (Result Http.Error (List Entry))
-
-
-type alias Model =
-    { packageCounts : List Entry
-    , error : Maybe String
-    }
-
-
-decodeEntry : Decoder Entry
-decodeEntry =
-    Json.Decode.map2 Entry
-        (field "date" date)
-        (field "count" float)
-
-
-decodePackageCounts : Json.Decode.Decoder (List Entry)
-decodePackageCounts =
-    (list decodeEntry)
 
 
 requestPackageCounts : Cmd Msg
@@ -82,41 +49,24 @@ displayError error =
     text error
 
 
-view : Model -> Html Msg
+view : Model -> Html.Html Msg
 view model =
-    main_
-        []
-        [ h1 [] [ text "Elm Package Counts" ]
-        , case model.error of
-            Just err ->
-                displayError err
+    toUnstyled <|
+        main_
+            [ css
+                [ displayFlex
+                , (alignItems center)
+                , flexDirection column
+                ]
+            ]
+            [ h1 [] [ text "Elm Package Counts" ]
+            , case model.error of
+                Just err ->
+                    displayError err
 
-            Nothing ->
-                chart model.packageCounts
-        ]
-
-
-chartConfig : List Entry -> LineChart.Config Entry Msg
-chartConfig entries =
-    { y = Axis.default 450 "Count" .count
-    , x = Axis.time 1270 "Date" (.date >> toTime)
-    , container = Container.default "line-chart-1"
-    , interpolation = Interpolation.default
-    , intersection = Intersection.default
-    , legends = Legends.default
-    , events = Events.default
-    , junk = Junk.default
-    , grid = Grid.default
-    , area = Area.default
-    , line = Line.default
-    , dots = Dots.default
-    }
-
-
-chart : List Entry -> Html Msg
-chart entries =
-    LineChart.viewCustom (chartConfig entries)
-        [ LineChart.line Colors.blue Dots.circle "Packages" entries ]
+                Nothing ->
+                    Svg.fromUnstyled (chart model.packageCounts)
+            ]
 
 
 subscriptions : Model -> Sub Msg
